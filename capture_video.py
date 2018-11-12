601758dcad16e420d53b4e71315fc4e9c4992dde
1-riverfish <15950513866@163.com>
2018-11-12 22:55:18 +0800

    add some fetures.

--- capture_video.py
+++ capture_video.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
 import cv2
 import numpy as np
+import sys
 
+# 命令行中传递的参数
 # time.txt中行数
-n = 10
+n = int(sys.argv[1])
+# 组号
+group_id = sys.argv[2]
+# 视频开始编号
+start = sys.argv[3]
+
 for i in range(0,n):
-    # name="4_4"+str(i)+".mp4"
-    name=str(i)+".mp4" 
+    start_part = int(start) + i
+    name=group_id+"_"+str(start_part)+".mp4"
+    print(name)
     videoCapture=cv2.VideoCapture(name)
 
     if (videoCapture.isOpened()):
         print('Open')
     else:
         print('Fail to open!')
+        exit()
 
     fps = videoCapture.get(cv2.CAP_PROP_FPS)  # 获取原视频的帧率
 
     size1 = (int(114), int(144))
     size2 = (int(577),int(450))# 自定义需要截取的画面的大小
     #size = (int(videoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),int(videoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))#获取原视频帧的大小
-    videoWriter1 = cv2.VideoWriter("hand"+str(i)+".avi", cv2.VideoWriter_fourcc(*'MJPG'), fps, size1)
-    videoWriter2 = cv2.VideoWriter("video"+str(i)+".avi", cv2.VideoWriter_fourcc(*'MJPG'), fps, size2)
+    videoWriter1 = cv2.VideoWriter("hand"+group_id+"_"+str(start_part)+".avi", cv2.VideoWriter_fourcc(*'MJPG'), fps, size1)
+    videoWriter2 = cv2.VideoWriter("video"+group_id+"_"+str(start_part)+".avi", cv2.VideoWriter_fourcc(*'MJPG'), fps, size2)
 
     success,frame = videoCapture.read()
 
--- cut_video.sh
+++ cut_video.sh
@@ -6,6 +6,11 @@ SOURCE_VIDEO_NAME="source.mp4"
 GROUPID=1
 PERSONID=0
 
+#用户输入基本信息
+echo "输出文件命名规则"
+read -p "请输入小组编号: " GROUPID
+read -p "请输入待处理视频开始编号: " PERSONID
+
 # 判断是否存在output文件夹
 if [ -d output ]
 then
@@ -15,8 +20,20 @@ else
     mkdir output
 fi
 
-echo "Cutting..."
+# 判断是否存在source.mp4文件
+if [ -f source.mp4 ]
+then
+    echo " source.mp4 file exists!"
+else
+    echo " source.mp4 file doesn't exist! Please move to current directory."
+    exit
+fi
+
+
+echo "开始切割..."
+# i为待处理视频在组内视频开始编号
 i=0
+i=$[$i+$PERSONID]
 for entry in $(cat ./time.txt)
 do
     # 从文件读取开始时间和视频时长
@@ -25,8 +42,8 @@ do
     echo "start-time:$START  time:$TIME"
 
     # 提取原始视频片段
-    # OUTPUTNAME="${GROUPID}_$PERSONID$i"
-    OUTPUTNAME=$i
+    OUTPUTNAME="${GROUPID}_$i"
+    echo $OUTPUTNAME
     ffmpeg -i "$SOURCE_VIDEO_NAME" -ss $START -t $TIME -c:v copy -c:a copy "./output/$OUTPUTNAME.mp4"
 
     # 从视频中分离音频
@@ -35,10 +52,13 @@ do
     i=$[$i+1]
 done
 
+i=$[ $i-$PERSONID ]
+
 # 执行python脚本　分离手语和视频
 cp capture_video.py ./output
 cd output
-./capture_video.py
+# 传入所在行参数
+python3 capture_video.py $i $GROUPID $PERSONID
 
 # 删除*.mp4
 rm -f *.mp4
